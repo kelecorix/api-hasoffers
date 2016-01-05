be9b87bb68e3408dd25b2785118aa210fc306845
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-01-05 10:13:33 +0200

    add new method

--- src/HasOffers/API/AdManager.hs
+++ src/HasOffers/API/AdManager.hs
@@ -5,10 +5,30 @@ import Data.Text
 
 --------------------------------------------------------------------------------
 
-addBrowserLanguageBlocked = 
+addCreative = 
   Call "AdManager"
        "addCreative"
        "POST"
        [ Param "campaign_id" True ""   -- Required
        , Param "data"        True ""   -- Required, An associative array of fields to the values that will be created.
+       ]
+
+createCampaign = 
+  Call "AdManager"
+       "createCampaign"
+       "POST"
+       [ Param "data"         True ""   -- Required, -- Required, An associative array of fields to the values that will be created.
+       , Param "return_object" True ""  -- boolean   
+       ]
+
+findAllCampaigns = 
+  Call "AdManager"
+       "findAllCampaigns"
+       "POST"
+       [ Param "filter"  False ""   
+       , Param "sort"    False ""
+       , Param "limit"   False ""
+       , Param "page"    False ""
+       , Param "fields"  False ""
+       , Param "contain" False ""  
        ]
