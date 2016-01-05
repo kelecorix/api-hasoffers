23d248dfddd7798479db4c279b78adfb3f510d12
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-01-05 10:08:46 +0200

    add new method

--- src/HasOffers/API/AdManager.hs
+++ src/HasOffers/API/AdManager.hs
@@ -3,3 +3,12 @@ module HasOffers.API.AdManager
 
 import Data.Text
 
+--------------------------------------------------------------------------------
+
+addBrowserLanguageBlocked = 
+  Call "AdManager"
+       "addCreative"
+       "POST"
+       [ Param "campaign_id" True ""   -- Required
+       , Param "data"        True ""   -- Required, An associative array of fields to the values that will be created.
+       ]
