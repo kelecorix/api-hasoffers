e34657fa57dbeaa359e3b75e61b5882ed4bb7ce9
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-01-06 10:02:56 +0200

    update methods

--- src/HasOffers/API/Offer.hs
+++ src/HasOffers/API/Offer.hs
@@ -13,3 +13,19 @@ addApprovalQuestion =
        "POST"
        [ Param "data" True ""
        ]
+
+addCategory = 
+  Call "Offer"
+       "addCategory"
+       "POST"
+       [ Param "id"          True ""
+       , Param "category_id" True "" 
+       ]
+
+addGroup = 
+  Call "Offer"
+       "addGroup"
+       "POST"
+       [ Param "id"       True ""
+       , Param "group_id" True ""  
+       ]
