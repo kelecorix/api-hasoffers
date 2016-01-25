7b610acac6c89fb9e4fdcf622893d8b347f0ad65
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-01-25 10:07:47 +0200

    update methods

--- src/HasOffers/API/Affiliate.hs
+++ src/HasOffers/API/Affiliate.hs
@@ -12,3 +12,25 @@ addAccountNote =
        [ Param "id"   True ""
        , Param "note" True "" 
        ]
+
+adjustAffiliateClicks = 
+  Call "Affiliate"
+       "adjustAffiliateClicks"
+       "POST"
+       [ Param "datetime"     True ""
+       , Param "affiliate_id" True ""
+       , Param "action"       True ""
+       , Param "quantity"     True ""
+       , Param "offer_id"     True ""
+       , Param "goal_id"      False ""
+       ]
+
+block = 
+  Call "Affiliate"
+       "block"
+       "POST"
+       [ Param "id"     True  ""
+       , Param "reason" False "" 
+       ]
+
+
