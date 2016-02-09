043ea2e324aa4d1046a3c26c97a7bd9b513371ae
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-02-09 10:04:42 +0200

    update Call builders

--- src/HasOffers/API/Common.hs
+++ src/HasOffers/API/Common.hs
@@ -31,3 +31,37 @@ data Param =
         , required :: Bool 
         , value    :: String
         } deriving (Show)
+
+-- | Convert action parameters
+--   to concatenated stringx
+buildParams :: [(String, String)] -> String
+buildParams [] = []
+buildParams p  =
+  concat $ buildParamsAux p
+    where buildParamsAux []           = []
+          buildParamsAux ((p1,p2):ps) =
+            case null p2 of
+              True  -> buildParamsAux ps
+              False ->
+                case p2=="-" of
+                  True  -> ("&" ++ p1 ++ "=")       : buildParamsAux ps
+                  False -> ("&" ++ p1 ++ "=" ++ p2) : buildParamsAux ps
+
+buildParams' :: [Param] -> String
+buildParams' [] = []
+buildParams' ps =
+  concat $ buildParamsAux ps
+    where buildParamsAux  []                = []
+          buildParamsAux ((Param n r v):ps) =
+            case r of
+              False ->
+                case null v of
+                  True  -> buildParamsAux ps
+                  False -> ("&" ++ n ++ "=" ++ v) : buildParamsAux ps
+              True  -> -- this is required field
+                ("&" ++ n ++ "=" ++ v) : buildParamsAux ps
+                  
+buildRequest :: String -> String -> String -> RequestBody -> IO Request
+buildRequest url ps meth body = do
+  nakedRequest <- parseUrl (url++ps)
+  return (nakedRequest { method = (BS.pack meth), requestBody = body })
