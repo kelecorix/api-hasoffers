dec14d57a64668a94e5ce6a946c7542fe0dfae39
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-02-26 11:14:22 +0200

    update methods

--- src/HasOffers/API/ApiKey.hs
+++ src/HasOffers/API/ApiKey.hs
@@ -11,3 +11,19 @@ import qualified Data.ByteString.Char8 as BS
 import HasOffers.API.Common
 
 --------------------------------------------------------------------------------
+
+findApiKeys = 
+  Call "ApiKey"
+       "findApiKeys"
+       "GET"
+       [ Param "filters"     False ""
+       ]
+
+generateApiKey = 
+  Call "ApiKey"
+       "generateApiKey"
+       "POST"
+       [ Param "user_type" True ""
+       , Param "user_id"   True ""  
+       ]
+
