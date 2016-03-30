54bb4b6862d11b59619b847815ecaf995ba43ce0
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-03-30 06:22:20 +0300

    update methods for ApiKey target

--- src/HasOffers/API/ApiKey.hs
+++ src/HasOffers/API/ApiKey.hs
@@ -12,42 +12,45 @@ import HasOffers.API.Common
 
 --------------------------------------------------------------------------------
 
-findApiKeys = 
+findApiKeys :: [Text] -> Call
+findApiKeys params = 
   Call "ApiKey"
        "findApiKeys"
        "GET"
-       [ Param "filters"     False ""
+       [ Param "filters" False getParam 0
        ]
-
+       
+findApiKeys :: [Text] -> Call
 generateApiKey = 
   Call "ApiKey"
        "generateApiKey"
        "POST"
-       [ Param "user_type" True ""
-       , Param "user_id"   True ""  
+       [ Param "user_type" True getParam 0
+       , Param "user_id"   True getParam 1  
        ]
 
+findApiKeys :: [Text] -> Call
 getUserApiKey =
   Call "ApiKey"
        "getUserApiKey"
        "GET"
-       [ Param "user_type" True ""
-       , Param "user_id"   True ""  
+       [ Param "user_type" True getParam 0
+       , Param "user_id"   True getParam 1  
        ]
 
 regenerateApiKey =
   Call "ApiKey"
        "getUserApiKey"
        "POST"
-       [ Param "user_type"        True ""
-       , Param "user_id"          True ""
-       , Param "existing_api_key" True ""  
+       [ Param "user_type"        True getParam 0
+       , Param "user_id"          True getParam 1
+       , Param "existing_api_key" True getParam 2  
        ]
 
 updateApiKeyStatus =
   Call "ApiKey"
        "updateApiKeyStatus"
        "GET"
-       [ Param "key_id" True ""
-       , Param "status" True ""
+       [ Param "key_id" True getParam 0
+       , Param "status" True getParam 1
        ]  
