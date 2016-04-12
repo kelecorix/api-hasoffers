3905c2eb487c7154e42fbf4ae42e15171aed4af0
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-04-12 06:24:43 +0300

    update methods

--- src/HasOffers/API/OfferFile.hs
+++ src/HasOffers/API/OfferFile.hs
@@ -68,3 +68,21 @@ findById params =
        , Param "fields"  False $ getParam params 1
        , Param "contain" False $ getParam params 2  
        ]
+
+getCreativeCode params = 
+  Call "OfferFile"
+       "getCreativeCode"
+       "GET"
+       [ Param "id"               True  $ getParam params 0   
+       , Param "tracking_url"     False $ getParam params 1
+       , Param "impression_pixel" False $ getParam params 2
+       , Param "etracking_url"    False $ getParam params 3
+       , Param "offer_id"         False $ getParam params 4
+       , Param "affiliate_id"     False $ getParam params 5
+       , Param "source"           False $ getParam params 6
+       , Param "aff_sub"          False $ getParam params 7
+       , Param "aff_sub2"         False $ getParam params 8
+       , Param "aff_sub3"         False $ getParam params 9
+       , Param "aff_sub4"         False $ getParam params 10
+       , Param "aff_sub5"         False $ getParam params 11  
+       ]
