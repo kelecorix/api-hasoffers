4176fee6c4ea148b5502bdd13bc19af2e0914518
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-04-14 06:31:22 +0300

    update methods

--- src/HasOffers/API/OfferPixel.hs
+++ src/HasOffers/API/OfferPixel.hs
@@ -11,3 +11,23 @@ import qualified Data.ByteString.Char8 as BS
 import HasOffers.API.Common
 
 --------------------------------------------------------------------------------
+
+create params = 
+  Call "OfferPixel"
+       "create"
+       "POST"
+       [ Param "data"          True $ getParam params 0
+       , Param "return_object" True $ getParam params 0  
+       ]
+
+findAll params = 
+  Call "OfferPixel"
+       "findAll"
+       "GET"
+       [ Param "filters" False $ getParam params 0   
+       , Param "sort"    False $ getParam params 1
+       , Param "limit"   False $ getParam params 2
+       , Param "page"    False $ getParam params 3
+       , Param "fields"  False $ getParam params 4
+       , Param "contain" False $ getParam params 5  
+       ]
