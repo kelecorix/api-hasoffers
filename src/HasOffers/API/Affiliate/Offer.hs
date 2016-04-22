1226d5ff3f95f1c48b1fe97109279e586a78956b
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-04-22 06:49:15 +0300

    update methods

--- src/HasOffers/API/Affiliate/Offer.hs
+++ src/HasOffers/API/Affiliate/Offer.hs
@@ -11,3 +11,24 @@ import qualified Data.ByteString.Char8 as BS
 import HasOffers.API.Common
 
 --------------------------------------------------------------------------------
+
+acceptOfferTermsAndConditions params = 
+  Call "Offer"
+       "acceptOfferTermsAndConditions"
+       "POST"
+       [ Param "offer_id" True $ getParam params 0
+       ]
+
+findAll params = 
+  Call "Offer"
+       "findAll"
+       "GET"
+       [ Param "filters" False $ getParam params 0   
+       , Param "sort"    False $ getParam params 1
+       , Param "limit"   False $ getParam params 2
+       , Param "page"    False $ getParam params 3
+       , Param "fields"  False $ getParam params 4
+       , Param "contain" False $ getParam params 5  
+       ]
+
+       
