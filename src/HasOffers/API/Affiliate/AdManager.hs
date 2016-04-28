fff09e08e8197a1ec9ed0147f3c491a96b4932fe
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-04-28 07:02:51 +0300

    update methods

--- src/HasOffers/API/Affiliate/AdManager.hs
+++ src/HasOffers/API/Affiliate/AdManager.hs
@@ -11,3 +11,18 @@ import qualified Data.ByteString.Char8 as BS
 import HasOffers.API.Common
 
 --------------------------------------------------------------------------------
+
+addCreative params = 
+  Call "Affiliate_AdManager"
+       "addCreative"
+       "POST"
+       [ Param "campaign_id" True $ getParam params 0  
+       , Param "data"        True $ getParam params 1   
+       ]
+
+createCampaign params = 
+  Call "Affiliate_AdManager"
+       "createCampaign"
+       "POST"
+       [ Param "data"          True $ getParam params 0 
+       ]
