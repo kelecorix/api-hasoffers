95db8deae7a0fb902281eb25e00997cdcd892116
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-04-30 07:51:49 +0300

    update methods

--- src/HasOffers/API/Affiliate/Affiliate.hs
+++ src/HasOffers/API/Affiliate/Affiliate.hs
@@ -11,3 +11,19 @@ import qualified Data.ByteString.Char8 as BS
 import HasOffers.API.Common
 
 --------------------------------------------------------------------------------
+
+findById  params = 
+  Call "Affiliate_Affiliate"
+       "findById"
+       "GET"
+       [ Param "id"      True $ getParam params 0
+       , Param "field"   False $ getParam params 1
+       , Param "value"   False $ getParam params 2
+       ]
+
+getAccountBalance  params= 
+  Call "Affiliate_Affiliate"
+       "getAccountBalance"
+       "GET"
+       [ 
+       ]
